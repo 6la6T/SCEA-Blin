124cf5181e3d823adcd429181441c35fa85fa448
EC2 Default User <ec2-user@ip-172-31-42-46.us-east-2.compute.internal>
2019-01-21 15:43:58 +0000

    bootstrap

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,23 @@
+class PageController < ApplicationController
+    
+    def home
+        
+    end 
+    
+    def btob
+        
+    end 
+    
+    def distributeur
+        
+    end
+    
+    def entreprise
+        
+    end 
+    
+    def contact 
+        
+    end
+    
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'page#home'
+    get 'distributeur', to: 'page#distributeur'
+    get 'btob', to: 'page#btob'
+    get 'entreprise', to: 'page#entreprise'
+    get 'contact', to: 'page#contact'
+end
